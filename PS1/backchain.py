22ca22089df629ba89b90b936b7bbb3a4f3178e1
czod <czod@yahoo.com>
2016-04-28 01:27:02 -0600

    More work on backchain.  Need to build goal tree and solve some stray Nones

--- PS1/backchain.py
+++ PS1/backchain.py
@@ -1,6 +1,7 @@
 from production import AND, OR, NOT, PASS, FAIL, IF, THEN, \
      match, populate, simplify, variables
 from zookeeper import ZOOKEEPER_RULES
+import string
 
 # This function, which you need to write, takes in a hypothesis
 # that can be determined using a set of rules, and outputs a goal
@@ -14,18 +15,35 @@ from zookeeper import ZOOKEEPER_RULES
 # tested on things other than ZOOKEEPER_RULES.
 
 
-def backchain_to_goal_tree(rules, hypothesis):
-for i in ZOOKEEPER_RULES:
-    consequent = i.consequent()
-    print consequent[0]
-    #print type(consequent[0])
-    mdict = match("(?x) is a (?y)",hypothesis)
-    if mdict['y'] in consequent[0]:
-        mdict['y']
-        print consequent[0]
-        ante = i.antecedent()
-        for j in ante:
-            print populate(j,mdict)
+def backchain_to_goal_tree(rules, hypothesis,rbridge = ''):
+    #print hypothesis
+    for s in string.split(hypothesis)[1:-1]:
+        rbridge += ' ' + s
+    #print rbridge
+    srule = "(?x)" + rbridge + " (?y)"
+
+    
+    for i in rules:
+        consequent = i.consequent()
+        
+        #print consequent[0]
+        #print type(consequent[0])
+        #print string.split(hypothesis)[1:-1]
+        
+        mdict = match(srule,hypothesis)
+        #print mdict
+        
+        if type(mdict['y']) == None or type(mdict['x']) == None:
+            #print "empty mdict"
+            return
+        
+        if mdict['y'] in consequent[0]:
+            #print consequent[0]
+            ante = i.antecedent()
+##            for j in ante:
+##            #print populate(j,mdict)
+            for k in ante:
+                backchain_to_goal_tree(rules,populate(k,mdict))
 # Here's an example of running the backward chainer - uncomment
 # it to see it work:
 #print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
