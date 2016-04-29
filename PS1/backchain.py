23b0857537efc275aebfa5259f920d4670a82110
czod <czod@yahoo.com>
2016-04-28 18:45:31 -0600

    spent lots of time reformmatting rules

--- PS1/backchain.py
+++ PS1/backchain.py
@@ -14,36 +14,63 @@ import string
 # specific to a particular rule set.  The backchainer will be
 # tested on things other than ZOOKEEPER_RULES.
 
+ACTIONS = [" has ",\
+           " is an ",\
+           " is a ",\
+           " is ",\
+           " gives ",\
+           " chews ",\
+           " could not ",\
+           " could ",\
+           " does not ",\
+           " does ",\
+           " lays "\
+           ]
 
-def backchain_to_goal_tree(rules, hypothesis,rbridge = ''):
-    #print hypothesis
-    for s in string.split(hypothesis)[1:-1]:
-        rbridge += ' ' + s
-    #print rbridge
-    srule = "(?x)" + rbridge + " (?y)"
+def backchain_to_goal_tree(rules, hypothesis,haction = '',hsub='',hobj = '',toprint = 1,hwords = []):
+    if toprint ==1: print "hypothesis is:  ",hypothesis
+    hwords = hypothesis.split()
+    for i in ACTIONS:
+        if i in hypothesis:
+            haction = i
+            hsub = hypothesis.split()[0]
+            replacer = hsub+i
+            hobj = hypothesis.replace(replacer,'')
+            
+            if toprint ==1: print "subject is:  ",hsub
+            if toprint ==1: print "predicate is:  ",haction
+            if toprint ==1: print "object is:  ",hobj
+            break
+    if haction == '':
+        hsub = hwords[0]
+        haction = ' '+ hwords[-1] + ' '
+        if toprint == 1:  print "hypothesis has no explicit object"
+        if toprint ==1: print "subject is:  ",hsub
+        if toprint ==1: print "predicate is:  ",haction
+
+    srule = "(?x)" + haction + "(?y)"
+    if toprint ==1: print "srule is:  ",srule
 
     
     for i in rules:
         consequent = i.consequent()
         
-        #print consequent[0]
-        #print type(consequent[0])
-        #print string.split(hypothesis)[1:-1]
+        if toprint ==1: print consequent[0]
+        if toprint ==2: print type(consequent[0])
+        if toprint ==2: print string.split(hypothesis)[1:-1]
         
-        mdict = match(srule,hypothesis)
-        #print mdict
-        
-        if type(mdict['y']) == None or type(mdict['x']) == None:
-            #print "empty mdict"
-            return
+        if match(consequent[0],hypothesis) != None:
+            mdict = match(srule,hypothesis)
+        else:
+            continue
+
+        if toprint ==1: print "variable bindings: ",mdict
         
         if mdict['y'] in consequent[0]:
-            #print consequent[0]
+            if toprint ==1: print consequent[0]
             ante = i.antecedent()
-##            for j in ante:
-##            #print populate(j,mdict)
             for k in ante:
                 backchain_to_goal_tree(rules,populate(k,mdict))
 # Here's an example of running the backward chainer - uncomment
 # it to see it work:
-#print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
+print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
